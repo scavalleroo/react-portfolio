3e82755b3c3da1de98fc578ea2599f448c4d3c9a
Alessandro Cavallotti <alessandro@junto.space>
2024-10-08 00:25:06 +0200

    New images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import AskToMe from './pages/AskToMe';
 import SelectedWork from './pages/SelectedWork';
